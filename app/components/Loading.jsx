f5a52df63c9b1bf9e434ff91ab701f8f56a6aacf
Matias Tellini <tellini.matias@gmail.com>
2024-01-08 21:06:23 -0300

    loading changed

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {BeatLoader } from 'react-spinners'
+import {PacmanLoader } from 'react-spinners'
 
 export const Loading = ({isLoading}) => {
   return  (
     <div className='bg-black h-screen w-[full] flex flex-col justify-center items-center'>
-        <BeatLoader   color="#36d7b7" loading={isLoading} />
+        <PacmanLoader   color="#36d7b7" loading={isLoading} />
         <p className='text-white'>Loading</p>
     </div>
   ) 
